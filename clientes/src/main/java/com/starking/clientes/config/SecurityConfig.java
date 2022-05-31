f40b50a45aa73fc02d3bd3fbfba888252012dfdf
pedrorhamon <pedrorhamon16@gmail.com>
2022-05-31 14:25:01 -0300

    configurando o securityConfig

---
+++ clientes/src/main/java/com/starking/clientes/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.starking.clientes.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+}
